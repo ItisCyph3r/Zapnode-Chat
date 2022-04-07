49aa6034c6ddd84aefba9008303869d22aa70a26
Momoh Samuel <samuelmomoh61@gmail.com>
2022-04-07 15:07:58 +0100

    heroku deploy 3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const port = 3000
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
@@ -23,6 +23,10 @@ io.on('connection', (socket) => {
     console.log('user disconnected');
   });
 });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(process.env.YOUR_PORT || process.env.PORT || port, () => {
+  console.log('listening on *: ' + port);
 });
+
+
+
+
